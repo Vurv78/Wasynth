9a0e15abb116cb5ae9af87bd85a92d19a5befb7c
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-08-17 12:34:06 +0100

    Use luau file extension

--- codegen/luau/src/lib.rs
+++ codegen/luau/src/lib.rs
@@ -1,5 +1,5 @@
-pub static RUNTIME: &str = include_str!("../runtime/runtime.lua");
-pub static EXPORT_RUNTIME: &str = include_str!("../runtime/export_runtime.lua");
+pub static RUNTIME: &str = include_str!("../runtime/runtime.luau");
+pub static EXPORT_RUNTIME: &str = include_str!("../runtime/export_runtime.luau");
 
 pub use translator::{from_inst_list, from_module_typed, from_module_untyped};
 
